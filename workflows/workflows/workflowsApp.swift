6bdccc5747353a389ff7b7c11bc3fcdb09327d8e
oscar canton <oscar.canton@vass.es>
2023-07-05 10:03:21 +0200

    initial commit

---
+++ workflows/workflows/workflowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  workflowsApp.swift
+//  workflows
+//
+//  Created by Oscar Canton on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct workflowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
