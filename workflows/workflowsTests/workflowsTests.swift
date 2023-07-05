2e982badca207f2ef44194bd5effe3d7cda55230
oscar canton <oscar.canton@vass.es>
2023-07-05 13:29:30 +0200

    tests

--- workflows/workflowsTests/workflowsTests.swift
+++ workflows/workflowsTests/workflowsTests.swift
@@ -31,7 +31,7 @@ final class workflowsTests: XCTestCase {
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
-            XCTAssertTrue("Hla" == "Hola")
+            XCTAssertTrue("Hola" == "Hola")
             // Put the code you want to measure the time of here.
         }
     }
